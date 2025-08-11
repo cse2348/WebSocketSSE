096c254230b9fefc48c13cf58ced61e396841136
seoyeon <2371013@ewhain.net>
2025-08-11 14:35:47 +0900

    first commit

---
+++ src/main/java/com/example/WebSocket_SSE/WebSocketSseApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebSocket_SSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketSseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebSocket_SSE/WebSocketSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebSocket_SSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
